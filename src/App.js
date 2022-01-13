34edef695c952a7039a633c889c581638195abfb
Narin Bannasan <impedanze@Narins-MacBook-Pro.local>
2022-01-13 15:39:59 +0700

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,111 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react';
+import './App.css'
+import './Trains'
+import Trains from './Trains';
+
+function Main(props) {
+  if (props.train != null && props.train != undefined) {
+    return (
+      <div className='App-main'>
+        <h3>Next train to go {props.destination.toUpperCase()}</h3>
+        <h2>{props.train.number} ({props.train.type})</h2>
+        <h3>Departs by</h3>
+        <h1><span className='App-hilight'>{props.train.departs}</span></h1>
+        <h3>Arrives by {props.train.arrives}</h3>
+      </div> 
+    );
+  } else {
+    return (
+      <div className='App-main'>
+        <h1>No trains<br/>availiable</h1>
+        <h4>Consider using the<br/>Airport Rail Link (ARL)</h4>
+      </div>
+    );
+  }
+};
+
+function UpcomingItems(props) {
+  return (
+    <>
+      <tr>
+        <td>No.{props.train.number}</td>
+        <td rowSpan='2'><b>{ props.train.departs }</b> to { props.train.arrives}</td>
+      </tr>
+      <tr>
+        <td> {props.train.type} </td>
+      </tr>
+    </>
+  );
+}
+
+function Upcoming(props) {
+  if (props.trains.length > 0) {
+    return (
+      <div className='App-upcoming'>
+      <h2>Upcoming trains...</h2>
+      <table>
+        <tbody>
+          {props.trains.map((train) => <UpcomingItems key={train.number} train={train} />)}
+        </tbody>
+      </table>
+      </div>
+    );
+  } else {
+    return (
+      <div className='App-upcoming'>
+        <p>No upcoming trains...</p>
+      </div>
+    );
+  }
+}
 
 function App() {
+  let [destination, setDestination] = useState('work');
+  let [nextTrain, setNextTrain] = useState(Trains.toWork[0]);
+  let [upcomingTrains, setUpcomingTrains] = useState(Trains.toWork);
+
+  function getNextTrains(destination) {
+    let currentTime = new Date();
+
+    let trainsArray = (destination === 'home') ? Trains.toHome : Trains.toWork;
+    let scheduledTrains = [];
+
+    trainsArray.forEach(train => {
+      let departureTime = new Date();
+      let [hours, minutes] = train.departs.split(':');
+      departureTime.setHours(hours);
+      departureTime.setMinutes(minutes);
+      departureTime.setSeconds(0);
+
+      if(currentTime < departureTime) {
+        scheduledTrains = [...scheduledTrains, train];
+      }
+    });
+    setDestination(destination);
+    setNextTrain(scheduledTrains[0])
+    setUpcomingTrains(scheduledTrains.slice(1));
+  }
+  
+  useEffect(() => {
+    getNextTrains('work');
+  }, []);
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='App-header'>
+        <h1>Narin K's Go2Work</h1>
+      </div>
+      <div className='App-selector'>
+        <button className='App-button' onClick={() => {getNextTrains('work')}}>Go work</button>
+        <button className='App-button' onClick={() => {getNextTrains('home')}}>Go home</button>
+      </div>
+      <Main train={nextTrain} destination={destination}/>
+      <Upcoming trains={upcomingTrains}/>
+      <br/>
+      <footer>
+        Developed by Narin K Bannasan<br/>
+        /thoughtworks
+      </footer>
     </div>
   );
 }
---
+++ src/Trains.js
@@ -0,0 +1,100 @@
+const Trains = {
+  "toWork" : [
+    {
+      "number" : 384,
+      "type" : "Ordinary",
+      "departs" : "06:02",
+      "arrives" : "06:40",
+      "unavailable" : ""
+    },{
+      "number" : 372,
+      "type" : "Ordinary",
+      "departs" : "07:04",
+      "arrives" : "07:45",
+      "unavailable" : ""
+    },{
+      "number" : 388,
+      "type" : "Express",
+      "departs" : "07:37",
+      "arrives" : "08:09",
+      "unavailable" : ""
+    },{
+      "number" : 278,
+      "type" : "Ordinary",
+      "departs" : "09:14",
+      "arrives" : "09:52",
+      "unavailable" : ""
+    },{
+      "number" : 280,
+      "type" : "Ordinary",
+      "departs" : "11:10",
+      "arrives" : "11:43",
+      "unavailable" : ""
+    },{
+      "number" : 284,
+      "type" : "Ordinary",
+      "departs" : "17:09",
+      "arrives" : "17:55",
+      "unavailable" : ""
+    },{
+      "number" : 276,
+      "type" : "Ordinary",
+      "departs" : "18:45",
+      "arrives" : "19:19",
+      "unavailable" : ""
+    }
+  ],
+  "toHome" : [
+    {
+      "number" : 275,
+      "type" : "Ordinary",
+      "departs" : "06:26",
+      "arrives" : "06:59",
+      "unavailable" : ""
+    },{
+      "number" : 283,
+      "type" : "Ordinary",
+      "departs" : "07:22",
+      "arrives" : "08:09",
+      "unavailable" : ""
+    },{
+      "number" : 367,
+      "type" : "Ordinary",
+      "departs" : "10:34",
+      "arrives" : "11:06",
+      "unavailable" : ""
+    },{
+      "number" : 279,
+      "type" : "Ordinary",
+      "departs" : "13:20",
+      "arrives" : "13:45",
+      "unavailable" : ""
+    },{
+      "number" : 277,
+      "type" : "Ordinary",
+      "departs" : "15:48",
+      "arrives" : "16:15",
+      "unavailable" : ""
+    },{
+      "number" : 391,
+      "type" : "Express",
+      "departs" : "17:17",
+      "arrives" : "17:48",
+      "unavailable" : ""
+    },{
+      "number" : 371,
+      "type" : "Ordinary",
+      "departs" : "18:06",
+      "arrives" : "18:38",
+      "unavailable" : ""
+    },{
+      "number" : 383,
+      "type" : "Ordinary",
+      "departs" : "18:47",
+      "arrives" : "19:21",
+      "unavailable" : ""
+    }
+  ]
+}
+
+export default Trains
